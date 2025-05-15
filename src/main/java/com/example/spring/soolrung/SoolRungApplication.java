c385286c1a3ddbc5c2b43cda79e3c7b412e1410a
Ninky0 <2020113392@dgu.ac.kr>
2025-05-15 21:12:27 +0900

    init : setting

---
+++ src/main/java/com/example/spring/soolrung/SoolRungApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.soolrung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoolRungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoolRungApplication.class, args);
+	}
+
+}
