ecee2898984f8db6e47f4febb28a0622e61c50a0
Ninky0 <2020113392@dgu.ac.kr>
2025-05-25 22:47:15 +0900

    docs : swagger docs initial settings

---
+++ src/main/java/com/example/spring/rmago/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.spring.rmago.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("'R-Ma-Go?' SpringBoot Swagger")
+                .description("얼마고 전체 로직")
+                .version("1.0.0");
+    }
+}
---
+++ src/main/java/com/example/spring/rmago/swagger/SearchLogControllerDocs.java
@@ -0,0 +1,7 @@
+package com.example.spring.rmago.swagger;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "SearchLog", description = "검색 Log 관련 API")
+public interface SearchLogControllerDocs {
+}
